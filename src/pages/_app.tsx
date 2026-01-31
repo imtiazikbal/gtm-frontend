6892eb3346e4c27f8262023456ac6c5e9fe41899
imtiazikbal <imtiaz.ikbal.com@gmail.com>
2026-01-31 23:40:20 +0600

    gtp include

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// pages/_app.tsx
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+import Head from 'next/head';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* 1. Standard Google Analytics tracking */}
+      <GoogleAnalytics trackPageViews gaMeasurementId="GTM-5M5VQSGZ" />
+
+      {/* 2. Global Head settings */}
+      <Head>
+        <title>GTM Automation Tester</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      {/* 3. The Main Page Content */}
+      <Component {...pageProps} />
+      
+     
+    </>
+  );
 }
