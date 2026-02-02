4c453342e975d19d0b1fe57488bf86816d92cdf6
imtiazikbal <imtiaz.ikbal.com@gmail.com>
2026-02-02 12:00:27 +0600

    gtm 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,34 +11,22 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>GTM Automation Tester</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        {/* Google Tag Manager - Script */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5M5VQSGZ');
-            `,
-          }}
-        />
-        {/* End Google Tag Manager - Script */}
       </Head>
 
       {/* Google Tag Manager - Noscript Fallback */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-5M5VQSGZ"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        />
-      </noscript>
+<Script
+            id="gtm-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5M5VQSGZ');
+          `,
+            }}
+          />
       {/* End Google Tag Manager - Noscript Fallback */}
 
       {/* 2. The Main Page Content */}
